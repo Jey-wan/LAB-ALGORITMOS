85073eba412e2836f4faf70c20a4d875961f549c
Jey-wan <142997148+Jey-wan@users.noreply.github.com>
2023-08-25 23:29:59 -0500

    Add files via upload

---
+++ LAB05ejercicio02.py
@@ -0,0 +1,10 @@
+frase = "Soy profesor y me encanta inspirar y enseñar a la gente"
+
+palabras = frase.split()
+
+palabras_unicas = set(palabras)
+
+cantidad_palabras_unicas = len(palabras_unicas)
+
+print("Frase:", frase)
+print("Cantidad de palabras únicas:", cantidad_palabras_unicas)
